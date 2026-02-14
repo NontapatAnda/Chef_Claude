6e5a14d43d6a44ace0d5dac6fe33c08ca7ce7143
Nontapat Toei - 6609612046 <andas4v4ge@gmail.com>
2026-02-14 13:48:44 +0700

    Add project

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+export default function Header() {
+    return (
+        <header> 
+            <img src="robot_chef.png" className="logo" alt="ChefRobot" />
+            <h1>Chef Claude</h1>
+        </header>
+    )
+}
---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+export default function Main() {
+    return (
+        <main>
+            <form className="add-ingredient-form">
+                <input type="text"
+                    placeholder="e.g. Omelette"
+                    aria-label="Add Ingredient"
+                 />
+                 <button>Add Ingredients</button>
+            </form>
+       
+        </main>
+    )
+}
