bdde07b0ce8c5d30f6e7b953b36c21152f9c27c9
Nontapat Toei - 6609612046 <andas4v4ge@gmail.com>
2026-02-16 15:12:26 +0700

    Add useState of adding ingredients to array

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,39 @@
+import { useState } from "react"
+
 export default function Main() {
+    const [ingredient,addIngredient] = useState([])
+
+
+    const ingredientLists = ingredient.map(ingredient => (
+        <li key={ingredient}>{ingredient}</li>
+
+
+    ))
+
+    function handleSubmit(event){
+
+        event.preventDefault()
+        const formData = new FormData(event.currentTarget)
+        const newingredient = formData.get("ingredient")
+        addIngredient(prevIngredient => [...prevIngredient,newingredient])
+
+    }
+
+
+
     return (
         <main>
-            <form className="add-ingredient-form">
+            <form onSubmit={handleSubmit}className="add-ingredient-form">
                 <input type="text"
                     placeholder="e.g. Omelette"
                     aria-label="Add Ingredient"
+                    name="ingredient"
                  />
                  <button>Add Ingredients</button>
             </form>
+            <ul>
+                {ingredientLists}
+            </ul>
        
         </main>
     )
