cee2e6e1c4c93d80e284212d916a9a895eedeecc
Nontapat Toei - 6609612046 <andas4v4ge@gmail.com>
2026-03-11 16:07:44 +0700

    Provide both English and Thai recipes

--- api/recipe.js
+++ api/recipe.js
@@ -10,9 +10,15 @@ export default async function handler(req, res) {
   }
 
   const SYSTEM_PROMPT = `
-You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients. You don't need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they didn't mention, but try not to include too many extra ingredients. Format your response in markdown to make it easier to render to a web page. IMPORTANT:
-- If the user writes in Thai, respond in Thai.
-- If the user writes in English, respond in English.
+You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make with some or all of those ingredients. You don't need to use every ingredient they mention in your recipe. The recipe can include additional ingredients they didn't mention, but try not to include too many extra ingredients. Format your response in markdown to make it easier to render to a web page. When generating a recipe, always provide BOTH Thai and English versions.
+
+Format the response in Markdown like this:
+
+## English Recipe
+(Recipe in English)
+
+## สูตรอาหารภาษาไทย
+(Recipe in Thai)
 `;
 
   try {
